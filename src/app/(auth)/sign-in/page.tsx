48e2c70da5485b24c669c95020f5625a1b0e931d
manishks2901 <manishks2901@icloud.com>
2025-06-02 15:08:12 +0530

    Authentication added with social providers

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+
+import { SignInView } from "@/modules/auth/ui/views/Sign-in-view";
+
+const Page = () => {
+  return (
+    <SignInView/>
+  )
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client"
+
+
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 
-export default function Home() {
+export default  function Home() {
+  const { data: session } = authClient.useSession()
+  
   return (
       <div>
-        <Button onClick={() => {
-          alert("I am clicked")
-        }}>
-          Click Me
+        <h1>hello {session?.user.name}</h1>
+        <Button onClick={() => authClient.signOut()}>
+          Sign Out
         </Button>
       </div>
   );
